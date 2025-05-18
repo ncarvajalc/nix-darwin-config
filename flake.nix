3be956985e8279d8a86b668925a477974a7e3864
Nicolás Carvajal <n.carvajalc@uniandes.edu.co>
2025-05-18 11:02:39 -0500

    added shell aliases config

--- flake.nix
+++ flake.nix
@@ -110,6 +110,11 @@
         enableFzfHistory = true;
       };
 
+      # Shell aliases 
+      environment.shellAliases = {
+        pgadmin-up ="docker run -d --name pgadmin --restart always -p 80:80 -e \"PGADMIN_DEFAULT_EMAIL=n.carvajalc@uniandes.edu.co\" -e \"PGADMIN_DEFAULT_PASSWORD=admin\" -v pgadmin_data:/var/lib/pgadmin dpage/pgadmin4";
+      };
+
       # Auto upgrade nix package and the deamon service
       services.nix-daemon.enable = true;
 
