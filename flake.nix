40e3ab292ce4a4bbd8a3db08f07ccaf01c3bedc4
Nicolás Carvajal <nicolascarvajal@Mac-mini-de-Nicolas.local>
2025-05-17 21:28:07 -0500

    initial commit with stable config

---
+++ flake.nix
@@ -0,0 +1,96 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    mac-app-util.url = "github:hraban/mac-app-util";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, mac-app-util, nix-homebrew }:
+  let
+    configuration = { pkgs, config, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.vim
+          pkgs.mkalias
+          pkgs.vscode
+          pkgs.obsidian
+        ];
+      
+      homebrew = {
+        enable = true;
+        brews = [
+          "mas"
+        ];
+        casks = [
+          "arc"
+          "rectangle"
+          "anydesk"
+          "logi-options+"
+          "maccy"
+          "alt-tab"
+          "raycast"
+          "chatgpt"
+          "cursor"
+        ];
+        masApps ={
+          "WhatsApp" = 310633997;
+        };
+        onActivation.cleanup = "zap";
+        onActivation.autoUpdate = true;
+        onActivation.upgrade = true;
+      };
+
+      system.defaults = {
+        dock.autohide = true;
+        NSGlobalDomain.KeyRepeat = 2;
+      };
+
+      # Auto upgrade nix package and the deamon service
+      services.nix-daemon.enable = true;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+      
+      # Allow unfree packages instalation
+      nixpkgs.config.allowUnfree = true;
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#Mac-mini-de-Nicolas
+    darwinConfigurations."Mac-mini-de-Nicolas" = nix-darwin.lib.darwinSystem {
+      modules = [ 
+        configuration
+        mac-app-util.darwinModules.default
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            # For Apple Silicon
+            enableRosetta = true;
+            # User owner of homebrew packages
+            user = "nicolascarvajal";
+          };
+        }
+      ];
+    };
+  };
+}
